4a6abe9027fd7bd988f461d01132d91b81692999
大塚健登 <kent0tsuka1118@gmail.com>
2022-05-02 21:09:57 +0900

    Add：保存したデータを自動ツイートする処理を実装

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,6 @@
 class TweetsController < ApplicationController
+  before_action :twitter_client, only: %i[create post]
+
   def new
     @tweet = Tweet.new
   end
@@ -12,8 +14,24 @@ class TweetsController < ApplicationController
     end
   end
 
+  def post
+    tweet = Tweet.first
+    @client.update(tweet.text)
+    redirect_to :root
+  end
+
   private
+
   def create_params
     params.require(:tweet).permit(:text)
+  end
+
+  def twitter_client
+    @client = Twitter::REST::Client.new do |config|
+      config.consumer_key =  ENV["CONSUMER_KEY"]
+      config.consumer_secret =  ENV["CONSUMER_SECRET_KEY"]
+      config.access_token =  ENV["ACCESS_TOKEN"]
+      config.access_token_secret =  ENV["ACCESS_TOKEN_SECRET"]
+    end
   end
 end
