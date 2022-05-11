0f0f0b9aad41cb1ddffb219e0a02eab465294105
大塚健登 <kent0tsuka1118@gmail.com>
2022-05-11 20:40:53 +0900

    Fix：フィードバックを終えて修正

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,7 +1,7 @@
 class WordsController < ApplicationController
   def show
     if params[:word_1].nil? || params[:word_2].nil? || params[:word_3].nil? || params[:word_1] == '' || params[:word_2] == '' || params[:word_3] == ''
-      @word = Word.find_by(text: '全部で11種類あリます！')
+      @word = Word.find_by(text: '全部で12種類あリます！')
     else
       words = params[:word_1] + params[:word_2] + params[:word_3]
       @word = Word.find_by(text: words)
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-Word.create(text: '全部で11種類あリます！', message: 'ちなみに全部つらい言葉です。')
+Word.create(text: '全部で12種類あリます！', message: '')
 Word.create(text: 'つらい', message: '開発者の一言：センスのあるクソアプリが思いつかなくてつらい。。。')
 Word.create(text: 'くらい', message: 'あなたは、暗い部屋が好きなようです。')
 Word.create(text: 'ふかい', message: 'あなたは、「何だこの不快なアプリは」と思ったはずです。')
@@ -10,4 +10,5 @@ Word.create(text: 'るざい', message: 'あなたは、時代が時代なら流
 Word.create(text: 'ふさい', message: 'あなたは、身内に負債を隠しているようです。')
 Word.create(text: 'むさい', message: 'あなたは、知らぬ間に無才を露呈してしまっているようです。')
 Word.create(text: 'ふらち', message: 'あなたは、気づかぬ間に不埒な行為をしてしまっているようです。')
+Word.create(text: 'むかち', message: 'このアプリを利用している時間自体が無価値です。')
 Word.create(text: '残念ながらハズレです。', message: 'つらい言葉が思いつかないのは良いことなので、このままアプリを閉じましょう！！')
